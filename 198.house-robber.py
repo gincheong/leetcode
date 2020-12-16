b2f8aeaa9290f7c1e82b8e24268817f03d2048fb
gincheong <gincheong2@gmail.com>
2020-12-16 17:40:47 +0900

    update

---
+++ 198.house-robber.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=198 lang=python3
+#
+# [198] House Robber
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # retry
+                    
+
+# @lc code=end
--- 53.maximum-subarray.py
+++ 53.maximum-subarray.py
@@ -7,18 +7,7 @@ from typing import List
 # @lc code=start
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # if len(nums) == 0:
-        #     return nums[0]
-
-        # subarray = []
-        # sub_sum = 0
-
-        # for i in range(len(nums)):
-        #     sub_sum = sub_sum + nums[i]
-        #     if sub_sum >= 0:
-        #         subarray.append(nums[i])
-        # print(subarray)
-
+        # retry
 
 # @lc code=end
 
