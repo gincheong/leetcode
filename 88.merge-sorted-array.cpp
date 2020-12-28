7f9831cf2aa3f6f6053dc092ed9097b5743c41b1
gincheong <gincheong2@gmail.com>
2020-12-28 12:39:42 +0900

    Update C++ Easy Problems

--- 88.merge-sorted-array.cpp
+++ 88.merge-sorted-array.cpp
@@ -3,10 +3,30 @@
  *
  * [88] Merge Sorted Array
  */
-
+#include <iostream>
+#include <vector>
+using namespace std;
 // @lc code=start
 class Solution {
  public:
-  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {}
+  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+    while (m > 0 && n > 0) {
+      if (nums1[m - 1] > nums2[n - 1]) {
+        nums1[m + n - 1] = nums1[m - 1];
+        m--;
+      } else {
+        nums1[m + n - 1] = nums2[n - 1];
+        n--;
+      }
+    }
+
+    if (m == 0) {
+      for (int i = 0; i < n; i++) {
+        nums1[i] = nums2[i];
+      }
+    }
+
+  }
 };
 // @lc code=end
