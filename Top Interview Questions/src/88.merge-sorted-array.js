901ecf311dc1962f83d77653b12b42ffa85fb239
gincheong <gincheong2@gmail.com>
2021-04-25 09:09:18 +0900

    디렉토리 변경

---
+++ Top Interview Questions/src/88.merge-sorted-array.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=88 lang=javascript
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+const merge = (nums1, m, nums2, n) => {
+  
+  while (m > 0 && n > 0) {
+    if (nums1[m - 1] >= nums2[n - 1]) {
+      nums1[m + n - 1] = nums1[m - 1];
+      m--;
+    } else {
+      nums1[m + n - 1] = nums2[n - 1];
+      n--;
+    }
+  }
+
+  if (n > 0) {
+    for (var i = 0; i < n; i++) {
+      nums1[i] = nums2[i];
+    }
+  }
+};
+// @lc code=end
+
